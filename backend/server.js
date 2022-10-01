e2bb1534609f746d77ade610a6c899b5714f9935
Mohammad Adnan <83416323+adnancoding@users.noreply.github.com>
2022-10-02 00:06:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-// for mongodb compass write below line in inverted commas in place of server link
-// mongodb://localhost:27017/netflix
-// mongodb+srv://adnan:4U0ReRerz4cxeQN3@cluster0.94jxy7f.mongodb.net/netflix?retryWrites=true&w=majority
+
 mongoose
   .connect("mongodb://localhost:27017/netflix", {
     useNewUrlParser: true,
